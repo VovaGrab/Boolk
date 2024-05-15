8b3cc66b0b6a7a4cf8f2a267aa388dc174010380
VovaGrab <114055237+VovaGrab@users.noreply.github.com>
2024-05-16 00:07:48 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Items from "./components/Items";
+import Categories from "./components/Categories";
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       favorites: [],
+      currentItems: [],
       competitions:[
       {
         id: 1,
         title: 'Best cloud for data storage',
         img: 'con1.jpg',
         desc: 'IT competition',
-        category: 'it-competition',
+        category: 'it',
         prize: '100$'
       },
       {
@@ -22,7 +24,7 @@ class App extends React.Component {
         title: 'Best business plan',
         img: 'con2.png',
         desc: 'Business competition',
-        category: 'business-competition',
+        category: 'business',
         prize: '150$'
       },
       {
@@ -30,7 +32,7 @@ class App extends React.Component {
         title: 'Best antivirus',
         img: 'con3.jpg',
         desc: 'IT competition',
-        category: 'it-competition',
+        category: 'it',
         prize: '1000$'
       },
       {
@@ -38,7 +40,7 @@ class App extends React.Component {
         title: 'Best application for servers',
         img: 'con4.webp',
         desc: 'IT competition',
-        category: 'it-competition',
+        category: 'it',
         prize: '1000$'
       },
       {
@@ -46,30 +48,44 @@ class App extends React.Component {
         title: 'Math competition',
         img: 'con5.webp',
         desc: 'Science competition',
-        category: 'science-competition',
+        category: 'science',
         prize: '500$'
       },
       {
         id: 6,
-        title: 'Writting competition',
+        title: 'Art competition',
         img: 'con6.png',
-        desc: 'Writting competition',
-        category: 'science-competition',
+        desc: 'Art competition',
+        category: 'art',
         prize: '200$'
       }
       ]
     }
+    this.state.currentItems = this.state.competitions
     this.addToFavorite = this.addToFavorite.bind(this)
     this.deleteFavorite = this.deleteFavorite.bind(this)
+    this.chooseCategory = this.chooseCategory.bind(this)
   }
   render() {
     return (
     <div className="wrapper">
       <Header favorites={this.state.favorites} onDelete={this.deleteFavorite} />
-      <Items competitions={this.state.competitions} onAdd={this.addToFavorite} />
+      <Categories chooseCategory={this.chooseCategory} />
+      <Items competitions={this.state.currentItems} onAdd={this.addToFavorite} />
       <Footer />
     </div>
     )
+  }
+
+  chooseCategory(category) {
+    if(category === 'all') {
+      this.setState({currentItems: this.state.competitions})
+      return
+    } 
+
+    this.setState({
+      currentItems: this.state.competitions.filter(el => el.category === category)    
+    })
   }
 
   deleteFavorite(id) {
